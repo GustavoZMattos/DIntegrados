49b9cf9145aa0c7c7380da1b3786a36b12ab4ca1
GustavoZMattos <49320228+GustavoZMattos@users.noreply.github.com>
2023-06-05 13:04:34 -0300

    Delete Servidor directory

--- Servidor/Models/ServerPerformance.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Server.Models
-{
-    public class ServerPerformance
-    {
-        public int MemoryUsageMb { get; set; }
-        public double CpuUsagePercent { get; set; }
-    }
-}
--- Servidor/Models/Signal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Server.Models
-{
-    public class Signal
-    {
-        public string User { get; set; }
-        public double Gain { get; set; }
-        public byte[] Data { get; set; }
-    }
-}
